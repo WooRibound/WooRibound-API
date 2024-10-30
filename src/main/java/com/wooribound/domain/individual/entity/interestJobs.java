39fb2270db02c1f28daeae774f085859df67fede
2oo1s <leejoowon0817@gmail.com>
2024-10-30 14:29:28 +0900

    feat : #4 resume/workHistory/interestJobs entity 추가

--- src/main/java/com/wooribound/domain/individual/entity/interestJobs.java
+++ src/main/java/com/wooribound/domain/individual/entity/interestJobs.java
@@ -1,4 +1,10 @@
 package com.wooribound.domain.individual.entity;
 
+import jakarta.persistence.Id;
+
 public class interestJobs {
+    @Id
+    private Long interestId;
+    private String userId;
+    private Long jobId;
 }
