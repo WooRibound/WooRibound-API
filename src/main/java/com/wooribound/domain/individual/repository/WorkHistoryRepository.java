7d1a6d28e83d14c28865dd4aeff0e12fc244e80e
leesj000603 <leesj000603@daum.net>
2024-11-03 22:13:48 +0900

    [chore] #20 - 안쓰는 import 삭제

--- src/main/java/com/wooribound/domain/individual/repository/WorkHistoryRepository.java
+++ src/main/java/com/wooribound/domain/individual/repository/WorkHistoryRepository.java
@@ -1,7 +1,6 @@
 package com.wooribound.domain.individual.repository;
 
 import com.wooribound.domain.individual.entity.WorkHistory;
-import lombok.extern.java.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
