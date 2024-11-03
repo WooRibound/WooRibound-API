f3d36edaa9fa86d7e8fcdd065d9c2afb427bb22a
leesj000603 <leesj000603@daum.net>
2024-11-03 22:24:53 +0900

    [feat] #20 - 일반 유저 로그인을 위한 인증정보와 서비스

---
+++ src/main/java/com/wooribound/global/security/WbUserDetail.java
@@ -0,0 +1,36 @@
+package com.wooribound.global.security;
+
+import com.wooribound.domain.individual.dto.OAuthDTO;
+import com.wooribound.global.constant.Gender;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@RequiredArgsConstructor
+public class WbUserDetail implements OAuth2User {
+  private final OAuthDTO oAuthDTO;
+
+  @Override
+  public Map<String, Object> getAttributes() {
+    return null;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.singleton(new SimpleGrantedAuthority("ROLE_WbUser"));
+  }
+
+  public String getId() {
+    return oAuthDTO.getId();
+  }
+
+  @Override
+  public String getName() {
+    return oAuthDTO.getName();
+  }
+}
