e5ea3c949eb9652ff3874cd20dec8726b0ee1ce6
leesj000603 <leesj000603@daum.net>
2024-11-03 22:17:48 +0900

    [chore] #20 - 안쓰는 import 삭제

--- src/main/java/com/wooribound/global/domain/userapply/UserApplyRepository.java
+++ src/main/java/com/wooribound/global/domain/userapply/UserApplyRepository.java
@@ -1,6 +1,5 @@
 package com.wooribound.global.domain.userapply;
 
-import lombok.extern.java.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserApplyRepository extends JpaRepository<UserApply, Long> {
