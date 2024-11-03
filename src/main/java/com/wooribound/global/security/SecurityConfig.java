6e8caf1ee6903528fe109aef24a790fd74ca66b6
leesj000603 <leesj000603@daum.net>
2024-11-03 22:26:03 +0900

    [feat] #20 - spring security 설정

--- src/main/java/com/wooribound/global/security/SecurityConfig.java
+++ src/main/java/com/wooribound/global/security/SecurityConfig.java
@@ -1,43 +1,91 @@
 package com.wooribound.global.security;
 
-import java.util.List;
+
+import com.wooribound.global.util.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+  private final JWTUtil jwtUtil;
 
-  private static final String[] SWAGGER_URLS = {
-      "/swagger-ui/**",
-      "/"
-  };
+  private final CustomSuccessHandler customSuccessHandler;
 
-  private static final String[] AUTH_REQUIRED_URLS = {
-      "/**"
-  };
+  private final WbUserDetailService wbUserDetailService;
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
-        .csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(
-            auth -> {
-              auth.requestMatchers(SWAGGER_URLS).permitAll();
-              auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-                  .permitAll();
-              auth.requestMatchers(AUTH_REQUIRED_URLS) // TODO : 로그인 필요한 uri 다 집어넣기
-                  .permitAll();
-              auth.anyRequest().permitAll();
-            }
-        )
-        .build();
-  }
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+    http
+        .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+          @Override
+          public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+            CorsConfiguration configuration = new CorsConfiguration();
+
+            configuration.setAllowedOrigins(Collections.singletonList("*")); // http://localhost:3000와 같이 주소로 허용가능
+            configuration.setAllowedMethods(Collections.singletonList("*"));
+            configuration.setAllowCredentials(true);
+            configuration.setAllowedHeaders(Collections.singletonList("*"));
+            configuration.setMaxAge(3600L);
+
+            // 우리쪽 서버에서 보낼때
+            configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+            configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+            return configuration;
+          }
+        }));
+
+    //csrf disable
+    http
+        .csrf((auth) -> auth.disable());
+
+    //From 로그인 방식 disable
+    http
+        .formLogin((auth) -> auth.disable());
+
+    //HTTP Basic 인증 방식 disable
+    http
+        .httpBasic((auth) -> auth.disable());
+
+    //JWT Filter
+    http
+        .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+    //oauth2
+    http
+        .oauth2Login(oauth2 -> oauth2
+            .successHandler(customSuccessHandler)
+            .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                .userService(wbUserDetailService)));
+
+    //경로별 인가 작업
+    http
+        .authorizeHttpRequests((auth) -> auth
+            .requestMatchers("/", "/**").permitAll()
+            .anyRequest().authenticated());
+
+    //세션 설정 : STATELESS
+    http
+        .sessionManagement((session) -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    return http.build();
+
+  }
 }
