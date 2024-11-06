6f44431178b2bdd96963f1c586745a9c4acc717c
2oo1s <leejoowon0817@gmail.com>
2024-11-06 18:14:29 +0900

    [fix] #35 - 충돌 해결 후, GlobalExceptionHandler 파일 수정

--- src/main/java/com/wooribound/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/wooribound/global/handler/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.wooribound.global.handler;
 
+import com.wooribound.global.exception.NoJobPostingException;
 import com.wooribound.global.exception.NoKnowhowException;
 import com.wooribound.global.exception.NoTokenException;
+import com.wooribound.global.exception.NoWbUserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,5 +22,16 @@ public class GlobalExceptionHandler {
   public  ResponseEntity<String> handleNoKnowhowException(NoKnowhowException e){
     return ResponseEntity.status(400).body(e.getMessage());
   }
+
+  @ExceptionHandler(NoWbUserException.class)
+  public  ResponseEntity<String> handleNoWbUserException(NoWbUserException e){
+    return ResponseEntity.status(400).body(e.getMessage());
+  }
+
+  @ExceptionHandler(NoJobPostingException.class)
+  public  ResponseEntity<String> handleNoJobPostingException(NoJobPostingException e){
+    return ResponseEntity.status(400).body(e.getMessage());
+  }
+
 }
 
