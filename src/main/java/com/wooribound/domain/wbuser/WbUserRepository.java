1537dc0c211a6f457b0caaef8817dea7ba0abacf
2oo1s <leejoowon0817@gmail.com>
2024-11-08 10:52:42 +0900

    [feat] #35 - 개인회원 목록 조회

--- src/main/java/com/wooribound/domain/wbuser/WbUserRepository.java
+++ src/main/java/com/wooribound/domain/wbuser/WbUserRepository.java
@@ -1,14 +1,24 @@
 package com.wooribound.domain.wbuser;
 
-import com.wooribound.domain.wbuser.WbUser;
+import com.wooribound.global.constant.YN;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface WbUserRepository extends JpaRepository<WbUser, String> {
 
-  Optional<WbUser> findByUserId(String userId);
+    Optional<WbUser> findByUserId(String userId);
+
+    @Query("SELECT DISTINCT w FROM WbUser w " +
+            "WHERE (:userName IS NULL OR w.name LIKE CONCAT('%', :userName, '%')) " +
+            "AND (:addrCity IS NULL OR w.addrCity = :addrCity)")
+    List<WbUser> findWbUsers(@Param("userName") String userName,
+                             @Param("addrCity") String addrCity);
+
 }
 
