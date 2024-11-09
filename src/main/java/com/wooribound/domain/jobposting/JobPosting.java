68b3d918debd7cc535f6b99a499c432961a0051c
2oo1s <leejoowon0817@gmail.com>
2024-11-09 12:17:39 +0900

    [chore] #63 - 지원현황과 알림 테이블 일대일 관계로 수정

--- src/main/java/com/wooribound/domain/jobposting/JobPosting.java
+++ src/main/java/com/wooribound/domain/jobposting/JobPosting.java
@@ -5,6 +5,7 @@ import com.wooribound.domain.userapply.UserApply;
 import com.wooribound.global.constant.PostState;
 import com.wooribound.domain.job.Job;
 
+import com.wooribound.global.constant.YN;
 import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,7 +17,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
 import java.util.List;
+
 import lombok.*;
 
 import java.util.Date;
@@ -29,43 +32,47 @@ import java.util.Date;
 @AllArgsConstructor
 @Entity
 @SequenceGenerator(
-    name = "job_posting_seq_generator",
-    sequenceName = "job_posting_SEQ",
-    allocationSize = 1
+        name = "job_posting_seq_generator",
+        sequenceName = "job_posting_SEQ",
+        allocationSize = 1
 )
 public class JobPosting {
-  @Id
-  @GeneratedValue(
-      strategy = GenerationType.AUTO,
-      generator = "job_posting_seq_generator"
-  )
-  @Column(name = "post_id")
-  private Long postId;
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "job_posting_seq_generator"
+    )
+    @Column(name = "post_id")
+    private Long postId;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "ent_id", nullable = false)
-  private Enterprise enterprise;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ent_id", nullable = false)
+    private Enterprise enterprise;
 
-  @Column(name = "post_title", nullable = false, length = 50)
-  private String postTitle;
+    @Column(name = "post_title", nullable = false, length = 50)
+    private String postTitle;
 
-  @Column(name = "post_img", nullable = false, length = 255)
-  private String postImg;
+    @Column(name = "post_img", nullable = false, length = 255)
+    private String postImg;
 
-  @Column(name = "start_date", nullable = false)
-  private Date startDate;
+    @Column(name = "start_date", nullable = false)
+    private Date startDate;
 
-  @Column(name = "end_date", nullable = false)
-  private Date endDate;
+    @Column(name = "end_date", nullable = false)
+    private Date endDate;
 
-  @Enumerated(value = EnumType.STRING)
-  @Column(name = "post_state", nullable = false, columnDefinition = "VARCHAR2(20) DEFAULT 'PENDING'")
-  private PostState postState  = PostState.PENDING;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "post_state", nullable = false, columnDefinition = "VARCHAR2(20) DEFAULT 'PENDING'")
+    private PostState postState = PostState.PENDING;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "job_id")
-  private Job job;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "VARCHAR2(1) DEFAULT 'N'")
+    private YN isDeleted = YN.N;
 
-  @OneToMany(mappedBy = "jobPosting",fetch = FetchType.LAZY)
-  private List<UserApply> userApply;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_id")
+    private Job job;
+
+    @OneToMany(mappedBy = "jobPosting", fetch = FetchType.LAZY)
+    private List<UserApply> userApply;
 }
