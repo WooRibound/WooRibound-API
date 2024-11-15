3e27155be7c467d07fc9d8bb5c08e1a1bf0103f0
2oo1s <leejoowon0817@gmail.com>
2024-11-15 15:46:43 +0900

    [chore] #35 - AdminAuthController swagger 설정 추가

--- src/main/java/com/wooribound/api/admin/controller/AdminAuthController.java
+++ src/main/java/com/wooribound/api/admin/controller/AdminAuthController.java
@@ -3,12 +3,15 @@ package com.wooribound.api.admin.controller;
 import com.wooribound.api.admin.dto.AdminApproveReqDTO;
 import com.wooribound.api.admin.facade.AdminAuthFacade;
 import com.wooribound.domain.admin.dto.AdminDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "서비스 관리자 권한 관련 API", description = "서비스 관리자 기능 중 가입/기업회원 가입 및 탈퇴 검증 입니다.")
 @RestController
 @RequestMapping("/admin/auth")
 @RequiredArgsConstructor
@@ -16,16 +19,19 @@ public class AdminAuthController {
 
     private final AdminAuthFacade adminAuthFacade;
 
+    @Operation(summary = "서비스 관리자 등록", description = "서비스 관리자 등록")
     @PostMapping("/join")
     public String join(@Validated @RequestBody AdminDTO adminDTO) {
         return adminAuthFacade.join(adminDTO);
     }
 
+    @Operation(summary = "기업회원 회원가입 검증", description = "기업회원 회원가입 검증")
     @PostMapping("/join-approval")
     public String joinApprove(@RequestBody AdminApproveReqDTO adminApproveReqDTO) {
         return adminAuthFacade.joinApprove(adminApproveReqDTO);
     }
 
+    @Operation(summary = "기업회원 회원탈퇴 검증", description = "기업회원 회원탈퇴 검증")
     @PostMapping("/delete-approval")
     public String deleteApprove(@RequestBody AdminApproveReqDTO adminApproveReqDTO) {
         return adminAuthFacade.deleteApprove(adminApproveReqDTO);
