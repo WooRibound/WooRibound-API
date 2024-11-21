36801e047096f47a2d3a0f241c0ea9f777d6e0db
soljjang777 <vivace8520vi@naver.com>
2024-11-21 18:01:28 +0900

    [fix] #115 - Filter에서 MultipartFile 읽음 이슈로 로그파일 필요한 API만 필터 사용하도록 수정

---
+++ src/main/java/com/wooribound/global/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.wooribound.global.config;
+
+import com.wooribound.global.filters.LoggingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig
+{
+    @Bean(name = "LoggingFilter")
+    public FilterRegistrationBean<LoggingFilter> loggingFilter() {
+        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LoggingFilter());
+        registrationBean.addUrlPatterns("/individualuser/jobposting", "/individualuser/jobposting/detail");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+}
