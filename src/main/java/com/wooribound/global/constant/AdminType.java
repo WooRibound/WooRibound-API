94b340d1b932949e16ec2fefb936cb7f2139f3c1
soljjang777 <vivace8520vi@naver.com>
2025-04-02 23:44:35 +0900

    [refactor] AdminType Enum 클래스 text필드 추가

--- src/main/java/com/wooribound/global/constant/AdminType.java
+++ src/main/java/com/wooribound/global/constant/AdminType.java
@@ -1,5 +1,14 @@
 package com.wooribound.global.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum AdminType {
-  ROLE_SERVICE_ADMIN , ROLE_INFRA_ADMIN
+
+  ROLE_SERVICE_ADMIN("서비스 관리자") ,
+  ROLE_INFRA_ADMIN("인프라 관리자");
+
+  private final String text;
 }
