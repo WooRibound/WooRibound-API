33ea2571e92eb064355d725ac7c60a8844cecea9
soljjang777 <vivace8520vi@naver.com>
2025-04-02 23:50:45 +0900

    [refactor] ApplyResult Enum 클래스 text필드 추가

--- src/main/java/com/wooribound/global/constant/ApplyResult.java
+++ src/main/java/com/wooribound/global/constant/ApplyResult.java
@@ -1,5 +1,16 @@
 package com.wooribound.global.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ApplyResult {
-  PENDING, ACCEPTED, REJECTED, CANCELED
+
+  PENDING("결과 대기중"),
+  ACCEPTED("합격"),
+  REJECTED("불합격"),
+  CANCELED("지원 취소");
+
+  private final String text;
 }
