36eb8fe4bf712a24e37a1acd6a87517c13390197
soljjang777 <vivace8520vi@naver.com>
2025-04-02 23:47:03 +0900

    [refactor] Gender Enum 클래스 text필드 추가

--- src/main/java/com/wooribound/global/constant/Gender.java
+++ src/main/java/com/wooribound/global/constant/Gender.java
@@ -1,5 +1,14 @@
 package com.wooribound.global.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Gender {
-  M, F, U
+  M("남자"),
+  F("여자"),
+  U("미지정");
+
+  private final String text;
 }
